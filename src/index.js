b8f3e37fed64c70f282b7e84089debe80bf7ab26
Biki Dey <deybiki721@gmail.com>
2024-11-25 18:56:39 +0530

    api response , error handling and app configuration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error on listening: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo db connection failed !!! ", error);
+  });
 
 /*
 import mongoose from "mongoose";
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,34 @@
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch((error) => {
+    next(error);
+  });
+};
+
+// Alternate ways
+
+// using arrow function
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res
+      .status(error.code || 500)
+      .send({ success: false, message: error.message });
+  }
+};
+
+// using regular function
+function asyncHandler(fn) {
+  return async function (req, res, next) {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      res
+        .status(error.code || 500)
+        .send({ success: false, message: error.message });
+    }
+  };
+}
+  
+*/
